7e99a3e53c9a4722f7ba2406e8b42a88ba6f6957
AlexOliveira11 <136550879+AlexOliveira11@users.noreply.github.com>
2024-03-06 19:22:43 -0300

    Update acessando-objetos.js

--- javascript-alura/acessando-objetos.js
+++ javascript-alura/acessando-objetos.js
@@ -1,6 +1,20 @@
-const objPessoa = {
+const estudante = {
     nome : 'José Silva',
     idade : 32,
     cpf : '123456789101',
     turma : 'JavaScript'
 }
+
+function exibeInfoEstudante(objetoEstudante, infoEstudante){
+
+    return estudante[infoEstudante]
+
+}
+
+console.log(estudante['nome'])
+console.log(estudante['cpf'])
+
+//  OU
+
+console.log(exibeInfoEstudante(estudante, 'nome'));
+console.log(exibeInfoEstudante(estudante, 'cpf'));
